0463e2a485daea19aa5c4e82c28f2a3b6e3073e2
Sven Luebke <svenoliver.luebke@elektrobit.com>
2022-01-27 00:48:39 +0100

    tried to put temporary files into configurable paths to avoid storing unneeded files (for SSDs)

--- hzn/url_printer.pl
+++ hzn/url_printer.pl
@@ -34,6 +34,8 @@ use JSON;
 use Time::Piece;
 use Time::Seconds;
 
+my $pathMANI = '/tmp/mani';
+
 # READ CHANNEL FILE
 my $channels;
 {
@@ -92,18 +94,18 @@ my @configname  = @{ $channels_data->{'config'} };
 #
 
 foreach my $configname ( @configname ) {
-	
+
 	# DEFINE IDs
 	my $new_id = $new_name2id->{$configname};
 	my $old_id = $old_name2id->{$configname};
-			
+
 	# FIND CHANNEL NAME IN NEW CHANNEL LIST
 	if( defined $new_id ) {
-		
+
 		if( $new_id ne $old_id) {
 			print STDERR "[ CHLIST WARNING ] CHANNEL \"$configname\" received new channel ID!\n";
 		}
-			
+
 		# DAY 1
 		if( $day_setting == 1 ) {
 			print "curl -s 'https://legacy-dynamic.oesp.horizon.tv/oesp/v2/XX/YYY/web/listings?byStationId=" . $new_id . "&byStartTime=" . $date1 . "~" . $date2 . "&sort=startTime&range=1-10000' | grep \"$new_id\" > mani/$new_id\n";
@@ -126,20 +128,20 @@ foreach my $configname ( @configname ) {
 		} elsif( $day_setting == 7 ) {
 			print "curl -s 'https://legacy-dynamic.oesp.horizon.tv/oesp/v2/XX/YYY/web/listings?byStationId=" . $new_id . "&byStartTime=" . $date1 . "~" . $date8 . "&sort=startTime&range=1-10000' | grep \"$new_id\" > mani/$new_id\n";
 		}
-		
+
 	# IF CHANNEL NAME WAS NOT FOUND IN NEW CHANNEL LIST: TRY TO FIND OLD ID IN NEW CHANNEL LIST
 	} elsif( defined $old_id ) {
-		
+
 		if( defined $new_id2name->{$old_id} ) {
 			my $renamed_channel = $new_id2name->{$old_id};
-			
+
 			if( defined $old_name2id->{$renamed_channel} ) {
 				print STDERR "[ CHLIST WARNING ] Renamed CHANNEL \"$renamed_channel\" (formerly known as \"$configname\") already exists in original channel list!\n";
 			} elsif( not defined $old_name2id->{$renamed_channel} ) {
 				print STDERR "[ CHLIST WARNING ] CHANNEL \"$configname\" received new channel name \"$renamed_channel\"!\n";
-			
+
 				my $renew_id = $new_name2id->{$renamed_channel};
-				
+
 				# DAY 1
 				if( $day_setting == 1 ) {
 					print "curl -s 'https://legacy-dynamic.oesp.horizon.tv/oesp/v2/XX/YYY/web/listings?byStationId=" . $renew_id . "&byStartTime=" . $date1 . "~" . $date2 . "&sort=startTime&range=1-10000' | grep \"$renew_id\" > mani/$renew_id\n";
@@ -163,12 +165,12 @@ foreach my $configname ( @configname ) {
 					print "curl -s 'https://legacy-dynamic.oesp.horizon.tv/oesp/v2/XX/YYY/web/listings?byStationId=" . $renew_id . "&byStartTime=" . $date1 . "~" . $date8 . "&sort=startTime&range=1-10000' | grep \"$renew_id\" > mani/$renew_id\n";
 				}
 			}
-			
+
 		# IF OLD ID WAS NOT FOUND IN NEW CHANNEL LIST
 		} else {
 			print STDERR "[ CHLIST WARNING ] CHANNEL \"$configname\" not found in new channel list!\n";
 		}
-	
+
 	# IF CHANNEL WAS NOT FOUND IN ANY CHANNEL LIST
 	} else {
 		print STDERR "[ CHLIST WARNING ] CHANNEL \"$configname\" not found in channel list!\n";
